676615458b97fabf3a3955d306168a35c77d16ee
Bartłomiej Świercz <bartek@rndity.com>
2023-03-08 11:38:27 +0100

    change: mapping +workspace/switch-to- to global state

--- +evil.el
+++ +evil.el
@@ -2,7 +2,20 @@
 
 (use-package! evil
   :config
-  (setq evil-want-fine-undo t))
+  (setq evil-want-fine-undo t)
+  (map! (:when IS-MAC
+          :g "s-t"   #'+workspace/new
+          :g "s-T"   #'+workspace/display
+          :g "s-1"   #'+workspace/switch-to-0
+          :g "s-2"   #'+workspace/switch-to-1
+          :g "s-3"   #'+workspace/switch-to-2
+          :g "s-4"   #'+workspace/switch-to-3
+          :g "s-5"   #'+workspace/switch-to-4
+          :g "s-6"   #'+workspace/switch-to-5
+          :g "s-7"   #'+workspace/switch-to-6
+          :g "s-8"   #'+workspace/switch-to-7
+          :g "s-9"   #'+workspace/switch-to-8
+          :g "s-0"   #'+workspace/switch-to-final)))
 
 (use-package! evil-owl
   :defer t
