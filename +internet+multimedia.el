9d5192b2a4e273e45109ed6434aa84ac3d689101
Bartłomiej Świercz <bartek@rndity.com>
2023-03-13 15:33:13 +0100

    new: adding 'read later' to elfeed

--- +internet+multimedia.el
+++ +internet+multimedia.el
@@ -7,9 +7,22 @@
   :config
   (defun google-translate--search-tkk () "Search TKK." (list 430675 2721866130)))
 
-(use-package empv
+(use-package! empv
   :defer t
   :config
   (setq empv-invidious-instance "https://inv.bp.projectsegfau.lt/api/v1")
   (add-to-list 'empv-mpv-args "--save-position-on-quit")
   )
+
+(use-package! elfeed
+  :defer t
+  :config
+  (map! :localleader
+        :map elfeed-search-mode-map
+        :desc "set/unset to read later" "l" (lambda ()
+                                              (interactive)
+                                              (elfeed-search-toggle-all 'readlater))
+        :map elfeed-show-mode-map
+        :desc "set to read later" "l" (lambda ()
+                                        (interactive)
+                                        (elfeed-show-tag 'readlater))))
